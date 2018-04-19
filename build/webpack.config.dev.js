88f0b51fc17bd24524552b58f7a479c88ea2095a
annecylee <annecylee@gmail.com>
2018-04-19 14:56:04 -0700

    Initial Commit a Template

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const utils = require('./utils')
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    hot: true
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'assets': utils.resolve('assets'),
+      'pages': utils.resolve('src/pages'),
+      'static': utils.resolve('static'),
+      'components': utils.resolve('src/components')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    }),
+    new CopyWebpackPlugin([{
+      from: utils.resolve('static/img'),
+      to: utils.resolve('dist/static/img'),
+      toType: 'dir'
+    }])
+  ]
+}
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <img src="~../../assets/img/coffee.svg">
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+@import "../../assets/app.scss";
+
+</style>
